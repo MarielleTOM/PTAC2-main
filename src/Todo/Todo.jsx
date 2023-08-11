5593f5b0ddc779e8f79d31abb55e2eddb691f7ec
MarielleTOM <marielle.martins@estudante.ifms.edu.br>
2023-08-11 16:55:29 -0400

    prooontoooo hehhheheh

---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+export default function Todo(){
+    return(
+        <div>
+        <h1>
+            Todo
+        </h1>
+        <Link to="/Home">Home</Link>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Home from './Home/Home.jsx';
+import Todo from './Todo/Todo.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,7 +10,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
           <Route path='/Home' element={<Home />}></Route>
+          <Route path='/Todo' element={<Todo />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
